06ca237e57a8f7e54da217fd4d8c0e3242042876
Michelle2317 <113144775+Michelle2317@users.noreply.github.com>
2024-12-04 03:58:46 -0800

    change view more on home page to navigate to top picks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -105,7 +105,7 @@ export default function Home() {
 				</h2>
 				<MuiLink
 					component={Link}
-					to='/recipes'
+					to='/topPicks'
 					sx={{
 						textDecoration: 'none',
 						color: 'var(--deep-pink)',
